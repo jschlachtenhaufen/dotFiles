d0e8d886945753134a1e4568bf2489c1f3249188
John Schlachtenhaufen <jschlach25@gmail.com>
2018-05-22 14:47:31 -0300

    Link.sh no longer links vim folder or README...

--- link.sh
+++ link.sh
@@ -1,11 +1,9 @@
 #/bin/bash
 # A script to sym link all the dot files into the home directory
 
-for file in ./*; do
-    if [ "$(basename $file)" != "$(basename $0)" ]; then
-        echo "Linking ${file:2} to ~/.${file:2}"
-        ln -is `pwd`/${file:2} ~/.${file:2}
-    fi
+for file in bash_aliases bashrc gitconfig vimrc; do
+    echo "Linking ${file} to ~/.${file}"
+    ln -is `pwd`/${file} ~/.${file}
 done
 
 git clone https://github.com/VundleVim/Vundle.vim.git ./vim/bundle/Vundle.vim
